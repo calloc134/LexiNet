e7090d3528c0991460b624645eae8d2cfb1f5199
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-08-19 02:40:19 +0000

    style: スタイル修正 メニュー追加

--- packages/frontend/src/_document.tsx
+++ packages/frontend/src/_document.tsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
 import { Outlet, Link } from "@tanstack/react-router";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, Avatar, Tooltip, Spacer } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuToggle,
+  Button,
+  Avatar,
+  Tooltip,
+  Spacer,
+  NavbarMenuItem,
+} from "@nextui-org/react";
 import { Login, Sun } from "tabler-icons-react";
 import { MetaMaskButton } from "@metamask/sdk-react";
 import { useAuthn } from "./lib/provider/authn/useAuthn";
@@ -28,52 +40,92 @@ export const Document = () => {
         <Navbar height="100px" maxWidth="full" className="flex-shrink-0 bg-primary/30 backdrop-blur-sm shadow-xl rounded-b-lg px-5" shouldHideOnScroll>
           {/* ナビゲーションバーの高さを固定 */}
           <NavbarBrand>
-            <Box sx={{ p: 1.5, pr: 2, pb: 1.8, borderRadius: 3 }} className={darkMode ? "bg-background" : ""}>
+            <Box sx={{ p: 1.5, pr: 2, pb: 1.8, borderRadius: 3 }} className={darkMode ? " hidden md:flex bg-background" : "md:flex"}>
               <Link to="/">
                 <img src={LogoImg} style={{ height: "45px", width: "270px" }} />
               </Link>
             </Box>
           </NavbarBrand>
-          <NavbarContent className="hidden sm:flex gap-12" justify="start">
-            <NavbarItem>
-              <Link to="/auth/chat">
-                <SideText primaryText="CHATS" secondaryText="チャット" />
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link to="/auth/users">
-                <SideText primaryText="USERS" secondaryText="ユーザ" />
-              </Link>
-            </NavbarItem>
-            <NavbarItem>
-              <Link to="/auth/transactions">
-                <SideText primaryText="TRANSACTION" secondaryText="トランザクション" />
-              </Link>
-            </NavbarItem>
+          <NavbarContent className="hidden xl:flex gap-12" justify="start">
+            <div className="hidden md:flex gap-12">
+              <NavbarItem>
+                <Link to="/auth/chat">
+                  <SideText primaryText="CHATS" secondaryText="チャット" />
+                </Link>
+              </NavbarItem>
+              <NavbarItem>
+                <Link to="/auth/users">
+                  <SideText primaryText="USERS" secondaryText="ユーザ" />
+                </Link>
+              </NavbarItem>
+              <NavbarItem>
+                <Link to="/auth/transactions">
+                  <SideText primaryText="TRANSACTION" secondaryText="トランザクション" />
+                </Link>
+              </NavbarItem>
+            </div>
           </NavbarContent>
           <NavbarContent justify="end">
-            <NavbarItem className="sm:flex gap-4">
-              <Tooltip content="Connect wallet" color="secondary" style={{ backgroundColor: "white" }}>
-                <Box className="hover:-translate-y-1 " sx={{ bgcolor: "#f66b0a", borderRadius: "10px" }}>
-                  <MetaMaskButton />
-                </Box>
-              </Tooltip>
-              <Tooltip content="スタイル切替" color="secondary">
-                <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
-                  <Sun size={20} />
-                </Button>
-              </Tooltip>
-              {isAuthenticated ? (
-                <Avatar isBordered as="button" className="transition-transform hover:-translate-y-1" color="secondary" name="Jason Hughes" size="md" />
-              ) : (
-                <Tooltip content="ログイン/登録" color="secondary">
-                  <Button variant="shadow" className="hover:-translate-y-1">
-                    <Link to="/auth/transactions">
-                      <Login size={20} />
-                    </Link>
+            <NavbarItem className="flex gap-4">
+              {/* 画面サイズが十分なとき */}
+              <div className="hidden sm:flex sm:gap-4">
+                <Tooltip content="Connect wallet" color="secondary" style={{ backgroundColor: "white" }}>
+                  <Box className="hover:-translate-y-1 " sx={{ bgcolor: "#f66b0a", borderRadius: "10px" }}>
+                    <MetaMaskButton />
+                  </Box>
+                </Tooltip>
+                <Tooltip content="スタイル切替" color="secondary">
+                  <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
+                    <Sun size={20} />
                   </Button>
                 </Tooltip>
-              )}
+                {isAuthenticated ? (
+                  <Avatar isBordered as="button" className="transition-transform hover:-translate-y-1" color="secondary" name="Jason Hughes" size="md" />
+                ) : (
+                  <Tooltip content="ログイン/登録" color="secondary">
+                    <Button variant="shadow" className="hover:-translate-y-1">
+                      <Link to="/auth/transactions">
+                        <Login size={20} />
+                      </Link>
+                    </Button>
+                  </Tooltip>
+                )}
+              </div>
+              {/* 画面サイズが十分でないとき */}
+              <div className="flex sm:hidden gap-4">
+                <NavbarMenuToggle />
+                <NavbarMenu>
+                  <NavbarMenuItem className="min-w-full flex gap-4">
+                    <Tooltip content="Connect wallet" color="secondary" style={{ backgroundColor: "white" }}>
+                      <Box className="hover:-translate-y-1 " sx={{ bgcolor: "#f66b0a", borderRadius: "10px" }}>
+                        <MetaMaskButton />
+                      </Box>
+                    </Tooltip>
+                  </NavbarMenuItem>
+                  <NavbarMenuItem className="justify-between flex gap-4">
+                    <Tooltip content="スタイル切替" color="secondary">
+                      <Button color="secondary" variant="shadow" onClick={toggleDarkMode} className="hover:-translate-y-1 ">
+                        <Sun size={20} />
+                      </Button>
+                    </Tooltip>
+                  </NavbarMenuItem>
+                  {isAuthenticated ? (
+                    <NavbarMenuItem>
+                      <Avatar isBordered as="button" className="transition-transform hover:-translate-y-1" color="secondary" name="Jason Hughes" size="md" />
+                    </NavbarMenuItem>
+                  ) : (
+                    <NavbarMenuItem>
+                      <Tooltip content="ログイン/登録" color="secondary">
+                        <Button variant="shadow" className="hover:-translate-y-1">
+                          <Link to="/auth/transactions">
+                            <Login size={20} />
+                          </Link>
+                        </Button>
+                      </Tooltip>
+                    </NavbarMenuItem>
+                  )}
+                </NavbarMenu>
+              </div>
             </NavbarItem>
           </NavbarContent>
         </Navbar>
--- packages/frontend/src/features/index/pages/Index.tsx
+++ packages/frontend/src/features/index/pages/Index.tsx
@@ -7,8 +7,8 @@ export const Index = () => {
     <>
       <div className="flex flex-col items-center justify-start h-screen">
         <img src={LogoImg} alt="LEXINET" className="w-unit-8xl" />
-        <h1 className="text-4xl font-bold">LEXINET</h1>
-        <p className="text-xl">誰かに頼らない思想を君に。</p>
+        <p className="text-xl">AI for ALL</p>
+        <p className="text-xl">AIの力を全ての人へ</p>
         <Spacer y={4} />
         <Button color="primary" variant="shadow" className="rounded-full hover:-translate-y-1">
           <Link to="/auth/chat">チャットにアクセス</Link>
